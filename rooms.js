49603aedc768cf73dcc403b6378177e610588e54
CivilianZero <Civilian.Protocol@gmail.com>
2016-10-19 11:27:34 -0400

    fixed bug with rooms not running constructors as constructors (hopefully)

--- rooms.js
+++ rooms.js
@@ -6,18 +6,16 @@ var entities = {
     i;
 
 function fromXToY(x, y) {
-    return Math.floor(Math.random() * (y-x)) + x;
-};
+    return Math.floor(Math.random() * (y  -x)) + x;
+}
 
 function generateRoom() {
     room = [];
     for (i = 0; i < fromXToY(8, 16); i++) {
-        let type = Math.random ? 'fauna' : 'flora'; // random 0 || 1
-        let set = entities[type]; // get entity arr
-        let n = fromXToY(0, set.length); // random 0 -> set.length
-        // create obj if fauna, otherwise use string identifier
-        let entity = type === 'fauna' ? new set[n] : set[n];
-        // push the entity
+        let type = Math.random ? 'fauna' : 'flora',
+            set = entities[type],
+            n = fromXToY(0, set.length),
+            entity = type === 'fauna' ? new set[n]() : set[n];
         room.push(entity);
     }
     return room;
