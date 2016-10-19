37fcc7497c7b117d2e03edfa21bbc6202f0aab0c
CivilianZero <Civilian.Protocol@gmail.com>
2016-10-19 09:21:39 -0400

    made changes to generateRoom (includes comments)

--- rooms.js
+++ rooms.js
@@ -1,10 +1,7 @@
-var entities = [
-    0, //fungus
-    1, //algae
-    2, //plant
-    3, //cat
-    4  //snail
-],
+var entities = {
+    flora: ['plant', 'fungus', 'algae'],
+    fauna: [Cat, Snail, Bat]
+},
     room = [],
     i,
     whichOnes;
@@ -14,31 +11,37 @@ function fromXToY(x, y) {
 };
 
 function generateRoom() {
-    room = [];
+    var room = [];
     for (i = 0; i < fromXToY(8, 16); i++) {
-        whichOnes = fromXToY(0, 5);
-        room.push(entities[whichOnes]);
+        let type = fromXToY(0, 1) ? 'fauna' : 'flora'; // random 0 || 1
+        let set = entities[type]; // get entity arr
+        let n = fromXToY(0, set.length); // random 0 -> set.length
+        // create obj if fauna, otherwise use string identifier
+        let entity = type === 'fauna' ? new set[n] : set[n];
+        // push the entity
+        room.push(entity);
     }
+    return room;
 }
 
-function populateRoom() {
-    for (i = 0; i < room.length; i++) {
-        switch(room[i]) {
-            case 0:
-                room[i] = 'fungus';
-                break;
-            case 1:
-                room[i] = 'algae';
-                break;
-            case 2:
-                room[i] = 'plant'
-                break;
-            case 3:
-                room[i] = new Bat();
-                break;
-            case 4:
-                room[i] = new Snail();
-                break;
-        }
-    }
-}
+//function populateRoom() {
+//    for (i = 0; i < room.length; i++) {
+//        switch(room[i]) {
+//            case 0:
+//                room[i] = 'fungus';
+//                break;
+//            case 1:
+//                room[i] = 'algae';
+//                break;
+//            case 2:
+//                room[i] = 'plant'
+//                break;
+//            case 3:
+//                room[i] = new Bat();
+//                break;
+//            case 4:
+//                room[i] = new Snail();
+//                break;
+//        }
+//    }
+//}
