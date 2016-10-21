581599e39fbadc25dd95ea9d0d1e312c6d147f28
Jordan Carroll <jfindlayc@gmail.com>
2016-10-21 11:58:34 -0400

    Added more text

--- main.js
+++ main.js
@@ -5,10 +5,27 @@ var player = new fauna.Kobold();
 var currentRoom = generateRoom();
 console.log(currentRoom);
 
+function printRoom() {
+    console.log("You see the following in the room with you: ");
+    for (var i = 0 ; i < currentRoom.length ; i++) {
+        if (currentRoom[i].id) {
+            console.log(currentRoom[i].id);
+        } else {
+            console.log(currentRoom[i]);
+        }
+    }
+}
+
 function takeInput() {
     var command;
     var eating;
     var thingToEat;
+    console.log("You are a kobold standing in a cave filled with with many things. There is a small mailbox here. What would you like to do? \n"
+                + "Options include: \n"
+                + "eat something \n"
+                + "leave room \n"
+                + "die \n")
+    printRoom();
     command = prompt(">> ");
     if (command.indexOf('eat ') === 0) {
         var eating = command.slice(4);
@@ -19,11 +36,11 @@ function takeInput() {
         } else {
             currentRoom.splice(currentRoom.indexOf(thingToEat), 1); 
         }
-        console.log(player.hunger);
-        console.log(currentRoom);
     } else if (command.indexOf('leave room') === 0) {
         currentRoom = generateRoom();
         console.log(currentRoom);
+    } else if (command.indexOf('mail') !== -1){
+        console.log("This is no time to check your mail!");
     } else if (command.indexOf('die') === 0) {
         player.kill();
     } else {
